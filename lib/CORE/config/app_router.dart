d82ede06a8cc6869743651e84de4d75b58354743
mahmudul4747 <mahmudulimran396@gmail.com>
2026-06-18 13:17:48 +0600

    file create

--- lib/CORE/config/app_router.dart
+++ lib/CORE/config/app_router.dart
@@ -1,8 +1,11 @@
 import 'package:go_router/go_router.dart';
-import 'package:my_project26/features/auth/presentation/pages/home_page.dart';
 
-import '../../features/auth/presentation/pages/login_page.dart';
-import '../../features/auth/presentation/pages/register_page.dart';
+import 'package:my_project26/features/auth/presentation/pages/login_page.dart';
+import 'package:my_project26/features/auth/presentation/pages/register_page.dart';
+import 'package:my_project26/features/auth/presentation/pages/home_page.dart';
+import 'package:my_project26/features/cart/presentation/pages/cart_page.dart';
+
+
 
 final GoRouter appRouter = GoRouter(
   initialLocation: '/login',
@@ -21,6 +24,11 @@ final GoRouter appRouter = GoRouter(
     GoRoute(
       path: '/home',
       builder: (context, state) => const HomePage(),
+    ),
+
+    GoRoute(
+      path: '/cart',
+      builder: (context, state) => const CartPage(),
     ),
   ],
 );
